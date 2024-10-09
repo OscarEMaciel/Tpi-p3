d4ff2acabf28ad15884fbdf01bf5ade310b14d48
lucia <luuli.miranda1@gmail.com>
2024-10-09 17:15:32 -0300

    admin service

---
+++ src/Application/Interfaces/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Domain.Entities;
+namespace Application.Interfaces;
+
+public interface IAdminService()
+{
+    public List<Admin> GetAdmins();
+    public Admin GetAdminById(int id);
+    public void AddAdmin(Admin admin);
+    public void UpdateAdmin(int id, Admin admin);
+    public void DeleteAdmin(int id);
+}
