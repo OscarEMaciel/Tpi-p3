7a3eea4edd18a28ee1b86d3161087d608005282a
OscarEMaciel <emaciel19@gmail.com>
2024-10-21 16:29:23 -0300

    program

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -2,6 +2,8 @@ using Application.Interfaces;
 using Application.Services;
 using Domain.Interfaces;
 using Infrastructure.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +25,19 @@ builder.Services.AddScoped<IAdminRepository, AdminRepository>();
 #endregion
 
 var app = builder.Build();
+
+// Configure the SQLite connection
+var connection = new SqliteConnection("Data Source=miWebAppDatabase.db");
+connection.Open();
+
+// Set journal mode to DELETE using PRAGMA statement
+using (var command = connection.CreateCommand())
+{
+    command.CommandText = "PRAGMA journal_mode = DELETE;";
+    command.ExecuteNonQuery();
+}
+
+builder.Services.AddDbContext<AppDbContext>(dbContextOptions => dbContextOptions.UseSqlite(connection));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
